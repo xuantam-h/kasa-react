124aee2ad90ace3d94061c0ae22567de8548b47e
XuanTam Hoang <xuantam.hoang23@gmail.com>
2023-07-11 08:10:24 +0200

    Edited Error component + styling

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,12 @@
+import {Link} from 'react-router-dom';
+import '../styles/Error.scss';
+
 const Error = () => {
     return (
       <div className="Error">
-        <h1>ERROR 404</h1>
+        <h1 className="big-heading">404</h1>
+        <p className="subtitle">Oups! La page que vous demandez n'existe pas.</p>
+        <Link to='/'>Retourner sur la page d’accueil</Link>
       </div>
     );
 }
