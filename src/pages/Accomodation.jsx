09a559d5654fe0efd20e7cff148d080373a4a997
XuanTam Hoang <xuantam.hoang23@gmail.com>
2023-08-08 16:35:01 +0200

    Fixed accomodation component + styling + markup

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -1,30 +1,44 @@
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+
 import Collapse from '../components/Collapse';
+import Slideshow from '../components/Slideshow';
+import Tag from '../components/Tag';
+
 import Accomodations from '../logements.json';
+import '../styles/Accomodation.scss';
 
 const Accomodation = () => {
 
+    /* Getting the id params in the URL */
     const {id} = useParams();
-    const [item, setItem] = useState([]);
-
-    useEffect(() => {
-      const currentItem = Accomodations.find((accomodation) => accomodation.id === id);
-      setItem(currentItem)
-    }, []);
+    /* Filtering to get the current accomodation page */
+    const currentAccomodation = Accomodations.find((accomodation) => accomodation.id === id);
 
     return (
         <div className="Accomodation container">
-            <h1 className="accomodation-title">{item.title}</h1>
-            <h2 className="accomodation-subtitle">{item.location}</h2>
-            <div className="accomodation-description">
+          <div className="accomodation-container">
+            <div className="accomodation-left">
+              <h1 className="accomodation-title">{currentAccomodation.title}</h1>
+              <h2 className="accomodation-subtitle">{currentAccomodation.location}</h2>
+              <ul className="accomodation-tags">
+                {currentAccomodation.tags.map((tag, index) => <li key={index} className="accomodation-tag">{tag}</li>)}
+              </ul>
+            </div>
+            <div className="accomodation-right">
+              <div className="accomodation-user">
+                <img src={currentAccomodation.host.picture} alt={currentAccomodation.host.name} />
+                <p className="accomodation-username">{currentAccomodation.host.name}</p>
+              </div>
+            </div>
+          </div>
+          <div className="accomodation-description">
               <Collapse 
                 title="Description"
-                content={item.description}
+                content={currentAccomodation.description}
               />
               <Collapse 
                 title="Equipements"
-                content={item.equipments}
+                content={currentAccomodation.equipments}
               />
             </div>
         </div>
